c6b28da8607061348b6f057f15e6d81d1fdeae21
abdelhedi <blg.abdelhedi@gmail.com>
2022-06-02 17:10:38 +0100

    Updates

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import "./assets/index.scss";
-import App from "./components/app";
-
-const root = document.getElementById("root");
-
-render(<App />, root);
